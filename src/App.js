1c7e4e722518be9f67949158d31103a0309d544c
nisevi <nicolas.s.vidal@gmail.com>
2018-01-31 08:29:36 +0100

    App component modified for using search_bar and video_list components.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyCDCkv6EWPJtwnP0eTHAyUmyJbKtx_QtBU'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+
+    YTSearch({key: API_KEY, term: 'agileventures'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +25,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <SearchBar/>
+        <VideoList videos={this.state.videos}/>
       </div>
     );
   }
