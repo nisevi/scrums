c620e5d401a445306b70915f612dee4c21c66924
nisevi <nicolas.s.vidal@gmail.com>
2018-01-31 16:08:12 +0100

    Adding proper key to element.

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -6,7 +6,7 @@ const VideoList = (props) => {
     return (
       <VideoListItem
         onVideoSelect={props.onVideoSelect}
-        key={video.etag}
+        key={video.id}
         video={video}/>
     );
   });
