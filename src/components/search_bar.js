09c02c15dbae407790a0fae4d23ccc1ffbc354b0
nisevi <nicolas.s.vidal@gmail.com>
2018-01-31 08:30:19 +0100

    Added components search_bar, video_list and video_list_item.

---
+++ src/components/search_bar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
+  render() {
+    return <input onChange={this.onInputChange}/>;
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+}
+
+export default SearchBar;
---
+++ src/components/video_list_item.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const VideoListItem = ({video}) => {
+  console.log(video);
+  return <li>Video</li>;
+}
+
+export default VideoListItem;
