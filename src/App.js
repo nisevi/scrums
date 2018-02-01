833dbd0a53f8b78e4c3dfa809abf552b478217e1
nisevi <nicolas.s.vidal@gmail.com>
2018-02-01 01:06:22 +0100

    Using a library to query videos of specific playlist.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import _ from 'lodash';
+import YTSearch from 'youtube-playlist-search';
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import YTSearch from 'youtube-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
@@ -20,16 +20,18 @@ class App extends Component {
     if (process.env.NODE_ENV === 'production') {
       this.key = process.env.REACT_APP_YTB_API_KEY_PROD
     }
-    this.opts = {
-      maxResults: 10,
+
+    this.params = {
+      part: 'snippet,contentDetails',
+      playlistId: 'PLH99prTh-VPqO7ld0o2Sny6bLxpf80Js0',
       key: this.key
     };
 
-    this.videoSearch('agileventures')
+    this.videoSearch('')
   }
 
   videoSearch(term) {
-    YTSearch(term, this.opts, (err, videos) => {
+    YTSearch(term, this.params, (err, videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
